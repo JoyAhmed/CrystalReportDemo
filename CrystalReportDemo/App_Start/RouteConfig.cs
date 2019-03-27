93f973115678a51bcc222ea68515accf4165f66a
JoyAhmed <joy_ahmed_007@yahoo.com>
2019-03-27 23:44:25 +0600

    Route Changed

--- CrystalReportDemo/App_Start/RouteConfig.cs
+++ CrystalReportDemo/App_Start/RouteConfig.cs
@@ -16,7 +16,7 @@ namespace CrystalReportDemo
             routes.MapRoute(
                 name: "Default",
                 url: "{controller}/{action}/{id}",
-                defaults: new { controller = "Home", action = "Index", id = UrlParameter.Optional }
+                defaults: new { controller = "Home", action = "EmployeeList", id = UrlParameter.Optional }
             );
         }
     }
